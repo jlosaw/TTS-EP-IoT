269aaf33af3bdda2de466829e2d873a32207ed38
jlosaw <jeremy.losaw@enventyspartners.com>
2018-11-18 20:47:31 -0500

    Create Math_Test.ino

---
+++ TTSIoT_Class2/Math_Test.ino
@@ -0,0 +1,35 @@
+void setup() {
+  int a = 2;
+  int b = 7;
+  int result;
+  float result_fl;
+
+  Serial.begin(9600);
+
+  Serial.print("Addition (a + b): ");
+  result = a + b;
+  Serial.println(result);
+
+  Serial.print("Subtraction (10 - 2): ");
+  result = 10 - 2;
+  Serial.println(result);
+
+  Serial.print("Multiplication (4 * 3): ");
+  result = 4 * 3;
+  Serial.println(result);
+
+  Serial.print("Int Division (5 / 4): ");
+  result = 5 / 4;
+  Serial.println(result);
+
+  Serial.print("Float Division (5.0 / 4.0): ");
+  result_fl = 5.0 / 4.0;
+  Serial.println(result_fl);
+
+  Serial.print("Remainder (11 % 4): ");
+  result = 11 % 4;
+  Serial.println(result);
+}
+
+void loop() {
+}
